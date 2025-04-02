9283aa59f7604848a1a05c88cf1a0118461a1c58
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2025-04-02 15:28:49 -0400

    Update app.py

--- Part4/app.py
+++ Part4/app.py
@@ -87,10 +87,14 @@ def load_default_model():
 
 # ----- Load Resources -----
 # For demonstration, limit to the first 5000 job postings.
-fine_tuned_embeddings = torch.tensor(load_fine_tuned_embeddings()[:5000], device=device)
-default_embeddings = torch.tensor(load_default_embeddings()[:5000], device=device)
-fine_tuned_embeddings = torch.tensor(fine_tuned_embeddings, dtype=torch.float32)
-default_embeddings = torch.tensor(default_embeddings, dtype=torch.float32)
+# fine_tuned_embeddings = torch.tensor(load_fine_tuned_embeddings()[:5000], device=device)
+# default_embeddings = torch.tensor(load_default_embeddings()[:5000], device=device)
+# fine_tuned_embeddings = torch.tensor(fine_tuned_embeddings, dtype=torch.float32)
+# default_embeddings = torch.tensor(default_embeddings, dtype=torch.float32)
+fine_tuned_array = np.array(load_fine_tuned_embeddings()[:5000], dtype=np.float32)
+fine_tuned_embeddings = torch.tensor(fine_tuned_array, dtype=torch.float32, device=device)
+default_array = np.array(load_default_embeddings()[:5000], dtype=np.float32)
+default_embeddings = torch.tensor(default_array, dtype=torch.float32, device=device)
 job_postings = load_job_postings()[:5000]
 fine_tuned_model = load_fine_tuned_model()
 default_model = load_default_model()
